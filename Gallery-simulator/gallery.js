edea6617561985579540b5c505d055ed91fcabdd
Jeremias ramirez <jereramirez499@gmail.com>
2019-02-26 21:16:35 -0400

    Refactorizando codigos

--- Gallery-simulator/gallery.js
+++ Gallery-simulator/gallery.js
@@ -1,32 +1,44 @@
 
 
-let over = document.getElementById("overlay")
-	let imgs = over.querySelectorAll('img')
-
+function setGallery(over){
+ 	// contenedor de las imagenes
+ 	let imgs = over.querySelectorAll('img');
 
 	for(let i=0;i<imgs.length; i++){
+// cuando a cada a unos de las imagenes
+	 imgs[i].addEventListener("click",(e)=>{
 
-	 imgs[i].addEventListener("click",function(e){
-	  
-	 	console.log(window.scrollY)
-
+	 	let valScroll = window.scrollY;
+	 	over.style.height = valScroll+'%';
+	 	   
+	 	  // boton de salir de la imagen abierta
 		 let closes= document.createElement("span")
 		 	 closes.setAttribute("class", "closes")
 		 	 closes.textContent = "X";
+
+		 	 // contenedor del overlay
 		let elementOver = document.createElement("div")
 			elementOver.setAttribute("class", "overlay")
 			elementOver.appendChild(closes)
-			elementOver.style.top=window.scrollY+'px';
+			elementOver.style.top=window.scrollY-50+'px';
+
 			let ims = document.createElement("img")
 				ims.src = e.target.src
 		
 				elementOver.appendChild(ims)
-
-				 closes.addEventListener("click", function(e){
+				// al darle click al boton cierra la imagen
+				 closes.addEventListener("click", (e)=>{
 				 	elementOver.remove(e.target)
+				 	 document.body.classList.remove("filter")
 				 })
+
 			document.body.insertBefore(elementOver,document.body.firstElementChild)
 			
 	},false)
+  }
+}
 
-	}
+let over = document.getElementById("overlay");
+
+
+setGallery(over);
